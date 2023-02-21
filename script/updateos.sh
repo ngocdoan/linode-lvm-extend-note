3839419bb6542352c15b57a9e0b4c3e8bd135baf
ngocdoan <vuongngocdoan@gmail.com>
2023-02-21 23:13:58 +0700

    Create updateos.sh

---
+++ script/updateos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu -y
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
