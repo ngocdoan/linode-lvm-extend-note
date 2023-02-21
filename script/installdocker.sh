319e3f60bedd51f650bdac6bcb815b6d58f5bcfa
ngocdoan <vuongngocdoan@gmail.com>
2023-02-21 23:13:38 +0700

    Create installdocker.sh

---
+++ script/installdocker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update && apt install git sudo unzip wget -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
