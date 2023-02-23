e52af37ab3afdf9f3281aad0a1acb22e875be631
ngocdoan <vuongngocdoan@gmail.com>
2023-02-23 23:06:57 +0700

    Update merge.py

--- merge/merge.py
+++ merge/merge.py
@@ -12,60 +12,37 @@ with open(csv_file_path, 'r') as csv_file:
     # Tạo list chứa các dictionary từ các dòng trong file CSV
     users = []
     for row in csv_reader:
-		
-		user = {
-			"profile_image": "profile-picture.png",
-			"username": row["username"],
-			"email": row["email"],
-			"auth_service": row["auth_service"],
-			"auth_data": row["auth_data"],
-			"password": row["password"],
-			"nickname": row["nickname"],
-			"first_name": row["first_name"],
-			"last_name": row["last_name"],
-			"position": row["position"],
-			"roles": row["roles"],
-			"locale": row["locale"],
-			"teams": [
-				{
-					"name": row["team-name"],
-					"theme": "{
-						\"awayIndicator\":\"#DBBD4E\",
-						\"buttonBg\":\"#23A1FF\",
-						\"buttonColor\":\"#FFFFFF\",
-						\"centerChannelBg\":\"#ffffff\",
-						\"centerChannelColor\":\"#333333\",
-						\"codeTheme\":\"github\",
-						\"linkColor\":\"#2389d7\",
-						\"mentionBg\":\"#2389d7\",
-						\"mentionColor\":\"#ffffff\",
-						\"mentionHighlightBg\":\"#fff2bb\",
-						\"mentionHighlightLink\":\"#2f81b7\",
-						\"newMessageSeparator\":\"#FF8800\",
-						\"onlineIndicator\":\"#7DBE00\",
-						\"sidebarBg\":\"#fafafa\",
-						\"sidebarHeaderBg\":\"#3481B9\",
-						\"sidebarHeaderTextColor\":\"#ffffff\",
-						\"sidebarText\":\"#333333\",
-						\"sidebarTextActiveBorder\":\"#378FD2\",
-						\"sidebarTextActiveColor\":\"#111111\",
-						\"sidebarTextHoverBg\":\"#e6f2fa\",
-						\"sidebarUnreadText\":\"#333333\",
-					}",
-					"roles": row["team_roles"],
-					"channels": [
-					  {
-						"name": row["channel-name"],
-						"roles": row["channel_role]",
-						"notify_props": {
-						  "desktop": row["notify_desktop"],
-						  "mark_unread": row["mark_unread"]
-						}
-					  }
-					]
-				}
-			]
-		}
+        
+        user = {
+            "profile_image": "profile-picture.png",
+            "username": row[0],
+            "email": row[1],
+            "auth_service": row[2],
+            "auth_data": row[3],
+            "password": row[4],
+            "nickname": row[5],
+            "first_name": row[6],
+            "last_name": row[7],
+            "position": row[8],
+            "roles": row[9],
+            "locale": row[10],
+            "teams": [
+                {
+                    "name": row[11],
+                    "roles": row[12],
+                    "channels": [
+                      {
+                        "name": row[13],
+                        "roles": row[14],
+                        "notify_props": {
+                          "desktop": row[15],
+                          "mark_unread": row[16]
+                        }
+                      }
+                    ]
+                }
+            ]
+        }
         users.append(user)
 
 # Tạo đường dẫn tới file JSON đầu ra
