6b3944d1570d1086fcb7e4f0b43c506498752604
ngocdoan <vuongngocdoan@gmail.com>
2023-02-23 23:14:43 +0700

    Update merge.py

--- merge/merge.py
+++ merge/merge.py
@@ -12,36 +12,38 @@ with open(csv_file_path, 'r') as csv_file:
     # Tạo list chứa các dictionary từ các dòng trong file CSV
     users = []
     for row in csv_reader:
-        
         user = {
-            "profile_image": "profile-picture.png",
-            "username": row[0],
-            "email": row[1],
-            "auth_service": row[2],
-            "auth_data": row[3],
-            "password": row[4],
-            "nickname": row[5],
-            "first_name": row[6],
-            "last_name": row[7],
-            "position": row[8],
-            "roles": row[9],
-            "locale": row[10],
-            "teams": [
-                {
-                    "name": row[11],
-                    "roles": row[12],
-                    "channels": [
-                      {
-                        "name": row[13],
-                        "roles": row[14],
-                        "notify_props": {
-                          "desktop": row[15],
-                          "mark_unread": row[16]
-                        }
-                      }
-                    ]
-                }
-            ]
+            "type": "user",
+            "user": {
+                "profile_image": "profile-picture.png",
+                "username": row[0],
+                "email": row[1],
+                "auth_service": row[2],
+                "auth_data": row[3],
+                "password": row[4],
+                "nickname": row[5],
+                "first_name": row[6],
+                "last_name": row[7],
+                "position": row[8],
+                "roles": row[9],
+                "locale": row[10],
+                "teams": [
+                    {
+                        "name": row[11],
+                        "roles": row[12],
+                        "channels": [
+                            {
+                                "name": row[13],
+                                "roles": row[14],
+                                "notify_props": {
+                                    "desktop": row[15],
+                                    "mark_unread": row[16]
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
         }
         users.append(user)
 
